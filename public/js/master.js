f6a3a98bacce73f9fee4fa74274cddce3d0f9457
Jack Jennings <jack@pokalo.net>
2013-03-05 17:28:39 -0500

    make slideshow clipped on x-axis

--- public/js/master.js
+++ public/js/master.js
@@ -1,10 +1,41 @@
 $(document).ready(function(){
-  var $projects = $('#projects')
-  
+  var $projects = $('#projects'),
+      $images = $('#project .images'),
+      $content = $('#content');
+
   $projects.imagesLoaded(function(){
     $(this).masonry({
       itemSelector: '.project',
       columnWidth: 320
     })
   })
+  
+  if ($images.length) {
+    
+    $images.wrap($('<div class="images_container"></div>'))
+    var $container = $('#project .images_container')
+    var ww = $(window).width()
+    $container.css({
+      overflow: 'hidden',
+      width: ww,
+      marginLeft: - (ww - $content.width()) / 2
+    })
+    $images.css({
+      paddingLeft: (ww - $content.width()) / 2
+    })
+    
+    $images.imagesLoaded(function(){
+      $images.css({
+        width: $images.find('.image, .video').sumWidth()
+      })
+    })
+  };
 })
+
+$.fn.sumWidth = function () {
+  sum = 0
+  $(this).each(function(){
+    sum += $(this).outerWidth()
+  })
+  return sum
+}
